f46739e5a03dfed234a4526dc69ff9588d470843
Luke Morton <luke@madebymade.co.uk>
2015-07-29 21:49:57 +0100

    Implement layouts

--- lib/pieces/builder.rb
+++ lib/pieces/builder.rb
@@ -5,8 +5,8 @@ module Pieces
   class Builder
     def build(config)
       Dir.chdir(config[:path]) do
-        p globals
-        save_files(routes.reduce([]) { |files, (name, route)| build_route(files, name, route) })
+        files = routes.reduce({}) { |files, (name, route)| build_route(files, name, route) }
+        save_files(render_files_in_layout(files))
       end
     end
 
@@ -16,41 +16,59 @@ module Pieces
       @route_config ||= YAML.load_file('config/routes.yml')
     end
 
+    def layout_path
+      "layouts/#{route_config['_layout']}.html.mustache"
+    end
+
     def globals
-      route_config.find { |key, _| key == '_global' }.last
+      route_config['_global'] or {}
     end
 
     def routes
-      route_config.reject { |key, _| key == '_global' }
+      route_config.reject { |key, _| key =~ /^_/ }
     end
 
     def build_route(files, name, route)
-      route['pieces'].reduce({}) do |files, piece|
+      route['pieces'].reduce(files) do |files, piece|
         piece, data = piece.keys.first, piece.values.first
 
         Dir["pieces/#{piece}/*"].each do |file|
           case File.extname(file)
           when '.mustache'
-            files["#{name}.html"] = { contents: '' } unless files.has_key?("#{name}.html")
+            files["#{name}.html"] = { contents: '', type: 'mustache' } unless files.has_key?("#{name}.html")
             files["#{name}.html"][:contents] << Mustache.render(File.read(file), globals.merge(data))
           when '.css'
-            files['compiled.css'] = { contents: '' } unless files.has_key?('compiled.css')
+            files['compiled.css'] = { contents: '', type: 'css' } unless files.has_key?('compiled.css')
             files['compiled.css'][:contents] << File.read(file)
           end
         end
 
         files.tap { |r| p r }
-      end.map { |(name, data)| { name: name }.merge(data) }
+      end
     end
 
     def save_files(files)
       FileUtils.rm_rf('build')
       Dir.mkdir('build')
-      files.each { |file| save_file(file) }
+      files.each { |name, file| save_file(name, file) }
     end
 
-    def save_file(file)
-      File.open("build/#{file[:name]}", 'w') { |f| f.write(file[:contents]) }
+    def save_file(name, file)
+      File.open("build/#{name}", 'w') { |f| f.write(file[:contents]) }
+    end
+
+    def render_in_layout(contents)
+      Mustache.render(File.read(layout_path), globals.merge(contents: contents))
+    end
+
+    def render_files_in_layout(files)
+      files.reduce({}) do |files, (name, file)|
+        if file[:type] == 'mustache'
+          file[:contents] = render_in_layout(file[:contents])
+        end
+
+        files.merge(name => file)
+      end
     end
   end
 end
