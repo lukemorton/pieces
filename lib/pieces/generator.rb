0bfd4cba456d5d4637439eb88aaec457f34067cb
Luke Morton <luke@madebymade.co.uk>
2015-07-30 09:14:56 +0100

    Tidy up

--- lib/pieces/generator.rb
+++ lib/pieces/generator.rb
@@ -2,7 +2,10 @@ module Pieces
   class Generator
     def init(config)
       FileUtils.mkdir_p(config[:path])
-      Dir["#{example_path}/{config,layouts,pieces}"].each { |dir| FileUtils.cp_r(dir, config[:path]) }
+
+      Dir["#{example_path}/{config,layouts,pieces}"].each do |dir|
+        FileUtils.cp_r(dir, config[:path])
+      end
     end
 
     private
