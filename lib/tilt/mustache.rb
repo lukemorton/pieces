a0dad1d0e27f2ee1ba58e3126da9d8aeddf330bf
Luke Morton <luke@madebymade.co.uk>
2015-08-06 22:28:20 +0100

    Fix mustache rendering in Tilt::MustacheTemplate

--- lib/tilt/mustache.rb
+++ lib/tilt/mustache.rb
@@ -7,7 +7,23 @@ module Tilt
     def prepare; end
 
     def evaluate(scope, locals, &block)
-      Mustache.render(data, locals.merge(scope.is_a?(Hash) ? scope : {}).merge(:yield => block.nil? ? '' : block.call))
+      Mustache.render(data, with_block(view_model(scope, locals), &block))
+    end
+
+    private
+
+    def view_model(scope, locals)
+      if scope.is_a?(Hash)
+        locals.merge(scope)
+      elsif scope.respond_to?(:to_h)
+        locals.merge(scope.to_h)
+      else
+        locals
+      end
+    end
+
+    def with_block(view_model, &block)
+      view_model.merge(:yield => block.nil? ? '' : block.call)
     end
   end
 end
