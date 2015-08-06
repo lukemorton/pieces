f132fa2db60bf87dd9acec04cad23ca1893b9e11
Luke Morton <luke@madebymade.co.uk>
2015-08-06 19:53:29 +0100

    Move example/ to examples/original/

--- spec/pieces/builder_spec.rb
+++ spec/pieces/builder_spec.rb
@@ -1,14 +1,14 @@
 describe Pieces::Builder do
-  before(:each) { described_class.new.build(path: 'example') }
-  after(:each) { FileUtils.rm_rf('example/build') }
+  before(:each) { described_class.new.build(path: 'examples/original/') }
+  after(:each) { FileUtils.rm_rf('examples/original/build') }
 
   context 'when building example site' do
-    subject { Dir['example/build/*'] }
+    subject { Dir['examples/original/build/*'] }
     it { is_expected.to_not be_empty }
   end
 
   context 'when compiling index.html from Mustache templates' do
-    let(:index_html) { File.read('example/build/index.html') }
+    let(:index_html) { File.read('examples/original/build/index.html') }
 
     def index_template(title, content)
       %Q{<article class="post">
@@ -26,7 +26,7 @@ describe Pieces::Builder do
   end
 
   context 'when building CSS' do
-    let(:css) { File.read('example/build/compiled.css') }
+    let(:css) { File.read('examples/original/build/compiled.css') }
 
     it 'should compile all CSS in pieces referenced in routes.yml' do
       expect(css).to eq(".post {\n  padding: 1em;\n}\n")
