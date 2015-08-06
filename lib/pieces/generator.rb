aa5466c902066e777e667b65f3551a781a69d0eb
Luke Morton <luke@madebymade.co.uk>
2015-08-06 22:06:52 +0100

    Generate pieces from boilerplate example

--- lib/pieces/generator.rb
+++ lib/pieces/generator.rb
@@ -6,12 +6,14 @@ module Pieces
       Dir["#{example_path}/{config,layouts,pieces}"].each do |dir|
         FileUtils.cp_r(dir, config[:path])
       end
+
+      FileUtils.cp("#{example_path}/Gemfile", "#{config[:path]}/Gemfile")
     end
 
     private
 
     def example_path
-      File.dirname(__FILE__) + '/../../example'
+      File.dirname(__FILE__) + '/../../examples/boilerplate'
     end
   end
 end
