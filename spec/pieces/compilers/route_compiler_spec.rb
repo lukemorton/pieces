b91472a288d0a1756b2140fa81f6e66123508dcd
Luke Morton <luke@madebymade.co.uk>
2015-08-08 14:36:27 +0100

    Add global example

--- spec/pieces/compilers/route_compiler_spec.rb
+++ spec/pieces/compilers/route_compiler_spec.rb
@@ -1,7 +1,7 @@
 describe Pieces::RouteCompiler do
   let(:compiler) { described_class.new(path: 'examples/original/') }
 
-  context 'when compiling index.html from Mustache templates' do
+  context 'when compiling about.html from Mustache templates' do
     let(:content) do
       [{ 'header' => {} },
        { 'posts/post' => { title: 'About Author',
@@ -10,8 +10,7 @@ describe Pieces::RouteCompiler do
     end
 
     let(:route_config) do
-      { '_global' => { 'title' => 'About' },
-        '_pieces' => [{ 'layouts/layout' => { '_pieces' => content } }] }
+      { '_pieces' => [{ 'layouts/layout' => { '_pieces' => content } }] }
     end
 
     subject do
@@ -21,5 +20,25 @@ describe Pieces::RouteCompiler do
     it { is_expected.to include('<h1 class="post__title">About Author</h1>') }
     it { is_expected.to include('<div class="post__content">') }
     it { is_expected.to include('<p>He is a cool dude.</p>') }
+  end
+
+  context 'when using global variables' do
+    let(:content) do
+      [{ 'header' => {} },
+       { 'posts/post' => {} },
+       { 'footer' => {} }]
+    end
+
+
+    let(:route_config) do
+      { '_global' => { 'title' => 'Global title' },
+        '_pieces' => [{ 'layouts/layout' => { '_pieces' => content } }] }
+    end
+
+    subject do
+      compiler.compile({}, :index, route_config)['index.html'][:contents]
+    end
+
+    it { is_expected.to include('<h1 class="post__title">Global title</h1>') }
   end
 end
