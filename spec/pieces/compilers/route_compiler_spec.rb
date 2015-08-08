b0d18d6bc380365f3f68fd0a06fabf99c7c8360a
Luke Morton <luke@madebymade.co.uk>
2015-08-08 15:03:13 +0100

    Improve testing of Pieces::RouteCompiler

--- spec/pieces/compilers/route_compiler_spec.rb
+++ spec/pieces/compilers/route_compiler_spec.rb
@@ -6,12 +6,27 @@ describe Pieces::RouteCompiler do
       compiler.compile({}, :index, route_config)['index.html'][:contents]
     end
 
-    context 'and nesting pieces' do
+    context 'with several pieces' do
       let(:content) do
-        [{ 'header' => {} },
+        [{ 'header' => { title: 'A header' } },
          { 'posts/post' => { title: 'About Author',
-                             content: '<p>He is a cool dude.</p>' } },
-         { 'footer' => {} }]
+                             content: '<p>He is a cool dude.</p>' } }]
+      end
+
+      let(:route_config) do
+        { '_pieces' => [{ 'layouts/layout' => { '_pieces' => content } }] }
+      end
+
+      it { is_expected.to include("<header>\n  A header\n</header>") }
+      it { is_expected.to include('<h1 class="post__title">About Author</h1>') }
+      it { is_expected.to include('<div class="post__content">') }
+      it { is_expected.to include('<p>He is a cool dude.</p>') }
+    end
+
+    context 'with nested pieces' do
+      let(:content) do
+        [{ 'posts' => { '_pieces' => [{ 'posts/post' => { title: 'About Author',
+                                                         content: '<p>He is a cool dude.</p>' } }] } }]
       end
 
       let(:route_config) do
@@ -26,9 +41,7 @@ describe Pieces::RouteCompiler do
     context 'when using global variables' do
       let(:route_config) do
         { '_global' => { 'title' => 'Global title' },
-          '_pieces' => [{ 'layouts/layout' => { '_pieces' => [{ 'header' => {} },
-                                                              { 'posts/post' => {} },
-                                                              { 'footer' => {} }] } }] }
+          '_pieces' => [{ 'layouts/layout' => { '_pieces' => [{ 'posts/post' => {} }] } }] }
       end
 
       it { is_expected.to include('<h1 class="post__title">Global title</h1>') }
@@ -38,9 +51,7 @@ describe Pieces::RouteCompiler do
       let(:route_config) do
         { '_global' => { 'title' => 'Global title' },
           '_pieces' => [{ 'layouts/layout' => { '_global' => { 'title' => 'Nested global title' },
-                                                '_pieces' => [{ 'header' => {} },
-                                                              { 'posts/post' => {} },
-                                                              { 'footer' => {} }] } }] }
+                                                '_pieces' => [{ 'posts/post' => {} }] } }] }
       end
 
       it { is_expected.to include('<h1 class="post__title">Nested global title</h1>') }
