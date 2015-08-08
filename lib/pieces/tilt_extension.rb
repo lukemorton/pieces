5c949e2e0f05409a5c76f74d5762c81eff3fe4d5
Luke Morton <luke@madebymade.co.uk>
2015-08-08 19:38:49 +0100

    Add support for tilt v1

--- lib/pieces/tilt_extension.rb
+++ lib/pieces/tilt_extension.rb
@@ -1,6 +1,14 @@
 require 'tilt'
 
 module Tilt
-  register_lazy :MustacheTemplate, 'tilt/mustache', 'mustache', 'ms'
-  register_lazy :CssTemplate, 'tilt/css', 'css'
+  if respond_to?(:register_lazy)
+    register_lazy :MustacheTemplate, 'tilt/mustache', 'mustache', 'ms'
+    register_lazy :CssTemplate, 'tilt/css', 'css'
+  else # support tilt v1
+    require 'tilt/mustache'
+    register MustacheTemplate, 'mustache', 'ms'
+
+    require 'tilt/css'
+    register CssTemplate, 'css'
+  end
 end
