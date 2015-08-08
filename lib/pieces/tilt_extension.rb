b769e1d6b69c48d4faf9a1ed3d6644de6eb825da
Luke Morton <luke@madebymade.co.uk>
2015-08-08 20:10:02 +0100

    Do not register custom tilt templates in rails < 5

--- lib/pieces/tilt_extension.rb
+++ lib/pieces/tilt_extension.rb
@@ -4,6 +4,8 @@ module Tilt
   if respond_to?(:register_lazy)
     register_lazy :MustacheTemplate, 'tilt/mustache', 'mustache', 'ms'
     register_lazy :CssTemplate, 'tilt/css', 'css'
+  elsif defined?(Rails)
+    # Register these yourself
   else # support tilt v1
     require 'tilt/mustache'
     register MustacheTemplate, 'mustache', 'ms'
