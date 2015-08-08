aec1d33029dc2f1e4e3aa7e569bfe833e88c4f79
Luke Morton <luke@madebymade.co.uk>
2015-08-08 09:21:50 +0100

    Simplify style compiler

--- lib/pieces/style_compiler.rb
+++ lib/pieces/style_compiler.rb
@@ -2,13 +2,10 @@ module StyleCompiler
   extend self
 
   def compile(files)
-    Dir["pieces/*/*.{css,scss,sass,less}"].each do |file|
-      files['compiled.css'] = { contents: '', type: 'css', compiled: [] } unless files.has_key?('compiled.css')
+    files['compiled.css'] = { contents: '', type: 'css' }
 
-      unless files['compiled.css'][:compiled].include?(file)
-        files['compiled.css'][:contents] << Tilt.new(file).render
-        files['compiled.css'][:compiled] << file
-      end
+    Dir["pieces/*/*.{css,scss,sass,less}"].each do |file|
+      files['compiled.css'][:contents] << Tilt.new(file).render
     end
   end
 end
