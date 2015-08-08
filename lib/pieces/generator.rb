db1d2b97e13e1334cc01726cc47dd9c7aefafc81
Luke Morton <luke@madebymade.co.uk>
2015-08-08 22:22:58 +0100

    Update generator to copy files if they don’t exist

--- lib/pieces/generator.rb
+++ lib/pieces/generator.rb
@@ -6,8 +6,18 @@ module Pieces
       path = config[:path] || Dir.pwd
       FileUtils.mkdir_p(path)
 
-      Dir["#{example_path}/{config,app}"].each do |dir|
-        FileUtils.cp_r(dir, path) unless Dir.exist?("#{path}/#{File.basename(dir)}")
+      if Dir.exist?("#{path}/app/views")
+        FileUtils.cp("#{example_path}/app/views/layouts/pieces.html.erb", "#{path}/app/views/layouts")
+        FileUtils.mkdir_p("#{path}/app/views/application")
+        FileUtils.cp("#{example_path}/app/views/application/header.html.erb", "#{path}/app/views/application")
+        FileUtils.cp("#{example_path}/app/views/application/footer.html.erb", "#{path}/app/views/application")
+      else
+        FileUtils.cp_r("#{example_path}/app/views", path)
+      end
+
+      unless File.exist?("#{path}/config/pieces.yml")
+        FileUtils.mkdir_p("#{path}/config")
+        FileUtils.cp("#{example_path}/config/pieces.yml", "#{path}/config/pieces.yml")
       end
 
       unless File.exist?("#{path}/Gemfile")
