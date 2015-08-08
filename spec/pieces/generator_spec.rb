a2fdf7efbd4fa8639facae7449859baef6decc36
Luke Morton <luke@madebymade.co.uk>
2015-08-08 13:20:11 +0100

    Ensure unspecified directory also tested

--- spec/pieces/generator_spec.rb
+++ spec/pieces/generator_spec.rb
@@ -6,6 +6,13 @@ describe Pieces::Generator do
       end
     end
 
+    shared_examples_for 'it is expected to contain built files' do
+      it { is_expected.to include('config/routes.yml') }
+      it { is_expected.to include('pieces/layouts/application.html.erb') }
+      it { is_expected.to include('pieces/application/header.html.erb') }
+      it { is_expected.to include('pieces/application/footer.html.erb') }
+    end
+
     context 'test_app/' do
       subject do
         described_class.init(path: 'test_app')
@@ -15,10 +22,19 @@ describe Pieces::Generator do
         end
       end
 
-      it { is_expected.to include('config/routes.yml') }
-      it { is_expected.to include('pieces/layouts/application.html.erb') }
-      it { is_expected.to include('pieces/application/header.html.erb') }
-      it { is_expected.to include('pieces/application/footer.html.erb') }
+      include_examples 'it is expected to contain built files'
+    end
+
+    context 'unspecified directory' do
+      subject do
+        Dir.mkdir('test_app')
+        Dir.chdir('test_app') do
+          described_class.init
+          Dir['**/*']
+        end
+      end
+
+      include_examples 'it is expected to contain built files'
     end
   end
 end
