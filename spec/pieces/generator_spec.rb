fcf148464186f0cb14d4b1b5374b150f40776a0a
Luke Morton <luke@madebymade.co.uk>
2015-08-08 13:31:55 +0100

    Move shared examples and tmp dir logic to spec_helper

--- spec/pieces/generator_spec.rb
+++ spec/pieces/generator_spec.rb
@@ -1,17 +1,6 @@
 describe Pieces::Generator do
   context 'when test_app is generated' do
-    around(:each) do |example|
-      Dir.mktmpdir do |dir|
-        Dir.chdir(dir, &example)
-      end
-    end
-
-    shared_examples_for 'it is expected to contain built files' do
-      it { is_expected.to include('config/routes.yml') }
-      it { is_expected.to include('pieces/layouts/application.html.erb') }
-      it { is_expected.to include('pieces/application/header.html.erb') }
-      it { is_expected.to include('pieces/application/footer.html.erb') }
-    end
+    within_tmp_dir
 
     context 'test_app/' do
       subject do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,24 @@ require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
 
 require 'pieces'
+
+shared_examples_for 'it is expected to contain built files' do
+  it { is_expected.to include('config/routes.yml') }
+  it { is_expected.to include('pieces/layouts/application.html.erb') }
+  it { is_expected.to include('pieces/application/header.html.erb') }
+  it { is_expected.to include('pieces/application/footer.html.erb') }
+end
+
+module TmpDir
+  def within_tmp_dir
+    around(:each) do |example|
+      Dir.mktmpdir do |dir|
+        Dir.chdir(dir, &example)
+      end
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.extend(TmpDir)
+end
