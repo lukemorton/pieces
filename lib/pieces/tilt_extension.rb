dd0e51213bbd2917586b9cd176f666839d4ba210
Luke Morton <luke@madebymade.co.uk>
2015-08-08 22:36:49 +0100

    Fix classes too

--- lib/pieces/tilt_extension.rb
+++ lib/pieces/tilt_extension.rb
@@ -2,8 +2,8 @@ require 'tilt'
 
 module Tilt
   if respond_to?(:register_lazy)
-    register_lazy :MustacheTemplate, 'pieces/tilt/mustache', 'mustache', 'ms'
-    register_lazy :CssTemplate, 'pieces/tilt/css', 'css'
+    register_lazy 'Pieces::Tilt::MustacheTemplate', 'pieces/tilt/mustache', 'mustache', 'ms'
+    register_lazy 'Pieces::Tilt::CssTemplate', 'pieces/tilt/css', 'css'
   else # support tilt v1
     require 'pieces/tilt/mustache'
     register Tilt::MustacheTemplate, 'mustache', 'ms'
