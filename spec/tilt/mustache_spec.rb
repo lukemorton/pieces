07cad378b6ca34a8ea5039b82c1431af8caf6d76
Luke Morton <luke@madebymade.co.uk>
2015-08-08 15:31:43 +0100

    Add tests for Tilt::MustacheTemplate

---
+++ spec/tilt/mustache_spec.rb
@@ -0,0 +1,18 @@
+describe Tilt::MustacheTemplate do
+  let(:template) { described_class.new('examples/original/pieces/posts/post.html.mustache') }
+
+  context 'when rendering with no data' do
+    subject { template.render }
+    it { is_expected.to include('<h1 class="post__title"></h1>') }
+  end
+
+  context 'when rendering with hash' do
+    subject { template.render(title: 'A title') }
+    it { is_expected.to include('<h1 class="post__title">A title</h1>') }
+  end
+
+  context 'when rendering with an object' do
+    subject { template.render(OpenStruct.new(title: 'Another title')) }
+    it { is_expected.to include('<h1 class="post__title">Another title</h1>') }
+  end
+end
