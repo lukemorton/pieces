c59342b7b259f57fd89e94327b68e429134e0382
Luke Morton <luke@madebymade.co.uk>
2015-08-15 13:16:01 +0100

    Remove unused CssTemplate from tilt extension

--- lib/pieces/tilt_extension.rb
+++ lib/pieces/tilt_extension.rb
@@ -3,12 +3,8 @@ require 'tilt'
 module Tilt
   if respond_to?(:register_lazy)
     register_lazy 'Pieces::Tilt::MustacheTemplate', 'pieces/tilt/mustache', 'mustache', 'ms'
-    register_lazy 'Pieces::Tilt::CssTemplate', 'pieces/tilt/css', 'css'
   else # support tilt v1
     require 'pieces/tilt/mustache'
     register Pieces::Tilt::MustacheTemplate, 'mustache', 'ms'
-
-    require 'pieces/tilt/css'
-    register Pieces::Tilt::CssTemplate, 'css'
   end
 end
