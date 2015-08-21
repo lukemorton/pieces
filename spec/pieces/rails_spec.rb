b3559cd8e3f6b4659652de6d8b3c1e9c25a9e38f
Luke Morton <luke@madebymade.co.uk>
2015-08-21 22:14:14 +0100

    Setup and clear down before rails tests

--- spec/pieces/rails_spec.rb
+++ spec/pieces/rails_spec.rb
@@ -1,5 +1,13 @@
 describe Pieces::Rails, type: :feature do
   context 'when booting rails server' do
+    before(:each) do
+      Pieces::Builder.new(path: 'examples/rails_app/').build
+    end
+
+    after(:each) do
+      FileUtils.rm_rf('examples/rails_app/build/')
+    end
+
     it 'should make styleguide available via routes' do
       visit '/styleguide'
       expect(page).to have_content 'Building systems with styleguides'
