9d944398c626f44c05af6ec10c62176cfe97d85b
Luke <luke@madebymade.co.uk>
2015-08-25 17:32:48 +0100

    Improve babel example

--- examples/babel/app/assets/javascripts/lazy.js
+++ examples/babel/app/assets/javascripts/lazy.js
@@ -1,3 +1,11 @@
 //= require ./vendor/load_css
+//= require ./vendor/webfontloader
+//= require_self
 
+loadCSS('/assets/lazy.css')
 
+WebFont.load({
+  google: {
+    families: ['Droid Sans', 'Droid Serif']
+  }
+})
--- examples/babel/app/assets/manifest.js
+++ examples/babel/app/assets/manifest.js
@@ -1,7 +1,9 @@
 // CSS bundles
+//
 //= link critical.css
 //= link lazy.css
-//
+
 // JS bundles
+//
 //= link app.js
 //= link lazy.js
